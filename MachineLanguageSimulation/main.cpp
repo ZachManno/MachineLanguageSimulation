3a2543b7dd34e58723596c7a50f23c46f5d9864d
Zach Manno <zdm5045@psu.edu>
2015-10-15 16:45:26 -0400

    Initial Commit

---
+++ MachineLanguageSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MachineLanguageSimulation
+//
+//  Created by Zach Manno on 10/15/15.
+//  Copyright (c) 2015 com.example. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
